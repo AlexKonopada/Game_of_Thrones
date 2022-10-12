cf01ba9a7f3cbb28526a66bf7911d263b70265e1
AlexKonopada <6761240@gmail.com>
2022-10-12 13:18:25 +0300

    changed branch

---
+++ src/main/java/lotr/Character.java
@@ -0,0 +1,42 @@
+package lotr;
+
+import kick.BasicKick;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public abstract class Character{
+
+    private int power;
+    private int hp;
+    private BasicKick kickType;
+
+    public void kick(Character c) {
+        kickType.kick(this, c);
+    }
+    public void setHp(int hp) {
+        if (hp >= 0)
+            this.hp = hp;
+        else
+            this.hp = 0;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public boolean isAlive() {
+        return this.getHp() > 0;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+    public String toString() {
+        return this.getClass().getSimpleName() + "{hp=" + this.hp +
+                ", power=" + this.power + "}";
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+}
---
+++ src/main/java/lotr/Hobbit.java
@@ -0,0 +1,11 @@
+package lotr;
+
+import kick.CryKick;
+import lombok.Getter;
+
+@Getter
+public class Hobbit extends Character{
+    public Hobbit() {
+        super(3, 0, new CryKick());
+    }
+}
